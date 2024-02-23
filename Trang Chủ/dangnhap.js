cfa3e81874060b9c8a85b75be82d10098affa035
Hung <hung11@gmail.com>
2024-02-23 10:01:26 +0700

    Initial commit

---
+++ Trang Chủ/dangnhap.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("formdangky");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  console.log(formData.get("name"));
+  console.log(formData.get("password"));
+});
+
+let result = text.link(
+  "http://127.0.0.1:5500/baicuoikhoa/Trang%20Ch%E1%BB%A7/exam.html"
+);
+document.getElementById("n").innerHTML = result;
---
+++ Trang Chủ/exam.js
@@ -0,0 +1,37 @@
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+const form = document.getElementById("formdangky");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+const openSidenav = document.getElementById("open-sidenav");
+
+const sidenav = document.getElementById("sidenav");
+let sidenavOpened = false;
+
+function openSidenavFunc() {
+  console.log("Clicked");
+  if (sidenavOpened == false) {
+    sidenav.classList.remove("invisible");
+    sidenavOpened = true;
+  } else if (sidenavOpened == true) {
+    sidenav.classList.add("invisible");
+    sidenavOpened = false;
+  }
+}
